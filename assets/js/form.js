747f0773074e6f54730a3217d84c72b5778f382a
Hannah Smith <hannahpsmith94@gmail.com>
2024-05-14 11:50:16 -0600

    updated form

---
+++ assets/js/form.js
@@ -0,0 +1,27 @@
+const blogUserName = document.querySelector('#username');
+const blogTitle = document.querySelector('#title');
+const blogContent = document.querySelector('#content');
+
+
+document.addEventListener(
+    'submit', function(event) {
+     event.preventDefault()
+     console.log(event)
+     addBlogPost()
+    }
+)
+
+function addBlogPost() {
+    const newBlogPost = {
+            title: blogTitle.value, content: blogContent.value, username: blogUserName.value
+        }
+    const currentBlogs = localStorage.getItem("blogs");
+    if (currentBlogs === null) {
+        const blogs = [newBlogPost];
+        localStorage.setItem('blogs', JSON.stringify(blogs));
+    } else {
+        const currentBlogsArray = JSON.parse(currentBlogs);
+        currentBlogsArray.push(newBlogPost);
+        localStorage.setItem('blogs', JSON.stringify(currentBlogsArray));
+    }
+}
